80f6d359cd5f6b2cb756713dca64bd62e11e1eac
Emmanuel Santos <edcancios82@gmail.com>
2026-02-13 08:09:46 +0000

    feat: Add sections endpoint

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/sections", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("./data.json"));
+  res.json(data.sections);
+});
+
+
 app.listen(3001, () => {
   console.log("Server running on port 3001");
 });
