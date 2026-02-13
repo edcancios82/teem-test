687b2538dae63f0f70ab0ba9e070d3666d5102cf
Emmanuel Santos <edcancios82@gmail.com>
2026-02-13 12:49:31 +0000

    feat: Added users endpoint

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/users", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("./data.json"));
+  res.json(data.users);
+});
+
 app.get("/sections", (req, res) => {
   const data = JSON.parse(fs.readFileSync("./data.json"));
   res.json(data.sections);
 });
 
+app.put("/sections/:id", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("./data.json"));
+  const section = data.sections.find(s => s.id === req.params.id);
+
+  section.formData = req.body;
+
+  fs.writeFileSync("./data.json", JSON.stringify(data, null, 2));
+  res.json(section);
+});
 
 app.listen(3001, () => {
   console.log("Server running on port 3001");
